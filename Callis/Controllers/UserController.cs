d598a754fff7006b71fef57373d395c090cbfa14
Torrike Whittaker <whittakertorrike@gmail.com>
2024-01-01 23:18:58 -0500

    Message to show success or fail

--- Callis/Controllers/UserController.cs
+++ Callis/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Callis.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Callis.Controllers
 {
@@ -27,13 +28,27 @@ namespace Callis.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult SignUp(User obj)
         {
-            obj.AccountTypeId = 2;
-            obj.TaskNo = 0;
-            var passwordHasher = new PasswordHasher<User>();
-            obj.PasswordHash = passwordHasher.HashPassword(obj, obj.PasswordHash);
-            _db.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index","Home");
+            try
+            {
+                obj.AccountTypeId = 2;
+                obj.TaskNo = 0;
+                var passwordHasher = new PasswordHasher<User>();
+                obj.PasswordHash = passwordHasher.HashPassword(obj, obj.PasswordHash);
+                _db.Add(obj);
+                _db.SaveChanges();
+                ViewBag.Message = "Account created successully";
+                ViewBag.Success = true;
+                ViewBag.Image = "~/images/372103860_CHECK_MARK_400px.gif";
+            }
+            catch (DbUpdateException)
+            {
+                ViewBag.Message = "Account creation failed";
+                ViewBag.Success = false;
+                ViewBag.Image = "~/images/372103860_CHECK_MARK_400px.gif";
+                throw;
+            }
+            
+            return View("SignUp");
         }
 
     }
--- Callis/Data/ApplicationDbContext.cs
+++ Callis/Data/ApplicationDbContext.cs
@@ -3,16 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Callis.Data;
 
-    public class ApplicationDbContext: DbContext
-    {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
-    {
-        
-    }
+    public class ApplicationDbContext: DbContext{
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+   
 
     public DbSet<Models.Task> Tasks { get; set; }
-    public DbSet<AccountType> AccountTypes { get; set; }
-    public DbSet<User> Users { get; set; }
-    public DbSet<Models.TaskStatus> TaskStatuses { get; set; }
+        public DbSet<AccountType> AccountTypes { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Models.TaskStatus> TaskStatuses { get; set; }
 }
 
